d53242a72393757d4cf471057615e5fa3d9fa6ad
Hamin Shim <alrbsshim14@gmail.com>
2023-07-29 16:55:38 +0900

    delete todo done

--- week2/script.js
+++ week2/script.js
@@ -21,11 +21,10 @@ function makeNewTodo(text) {
 
   todoContaiiner.prepend(newTodoBox);
 }
-makeNewTodo("1번째 할 일");
-makeNewTodo("2번째 할 일");
+// makeNewTodo("1번째 할 일");
+// makeNewTodo("2번째 할 일");
 
 const checkboxes = document.querySelectorAll("input.checkbox");
-console.log(checkboxes);
 function toggleTodo(e) {
   const toggledTodo = e.target.parentElement;
   console.log(toggledTodo);
@@ -34,15 +33,31 @@ function toggleTodo(e) {
   toggleTodoText.classList.toggle("done");
 }
 checkboxes.forEach((element) => element.addEventListener("change", toggleTodo));
+
 const todoForm = document.getElementsByTagName("form")[0];
-console.log(todoForm);
-todoForm.addEventListener("submit", (e) => e.preventDefault());
-const submitBtn = document.getElementById("submitBtn");
-const inputTodo = document.getElementById("newTodoText");
-console.log(inputTodo.innerHTML);
-submitBtn.addEventListener("click", () => {
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
   const newTodoText = inputTodo.value;
   makeNewTodo(newTodoText);
   inputTodo.value = "";
 });
-console.log(submitBtn);
+const inputTodo = document.getElementById("newTodoText");
+
+const trashIcons = document.querySelectorAll(".trash");
+function deleteTodo(e) {
+  const toggledTodo = e.target.parentElement;
+  const toggleTodoText = toggledTodo.children[1].innerHTML;
+  console.log(toggleTodoText);
+  const ok = confirm("[" + toggleTodoText + "] 할 일을 삭제하시겠습니까?");
+  if (ok) {
+    const todoContainer = document.querySelector(".todoContainer");
+    todoContainer.removeChild(toggledTodo);
+    console.log(todoContainer.children.length);
+    if (todoContainer.children.length == 1) {
+      console.log("hi");
+      const emptyNotice = document.querySelector(".emptyNotice");
+      emptyNotice.classList.toggle("hide");
+    }
+  }
+}
+trashIcons.forEach((element) => element.addEventListener("click", deleteTodo));
