45a70855da2cd7976aaac879418fee363cb59b3e
alrbs10 <alrbsshim14@gmail.com>
2023-08-14 19:46:33 +0900

    ..ing

--- week3/practice/src/App.js
+++ week3/practice/src/App.js
@@ -1,35 +1,136 @@
 import { useState } from "react";
-
+import "./App.scss";
 function App() {
-  const [cnt, setCnt] = useState(0);
-  const helloString = "HELLOOO";
-  const Hello = () => (
-    <div className="hello" onClick={() => console.log("click hello")}>
-      Hello!
-      {helloString}
-    </div>
-  );
+  const [mode, setMode] = useState(null);
+  const colors = ["red", "green", "blue"];
+  const [logs, setLogs] = useState([]);
   const Counter = () => {
+    const [cnt, setCnt] = useState(0);
     return (
-      <div className="counterContainer" style={{ display: "flex" }}>
-        <button className="plus" onClick={() => setCnt(cnt + 1)}>
-          +
-        </button>
-        <div className="count" style={{ margin: "0 10px" }}>
-          {cnt}
+      <div>
+        <h1 style={{ textAlign: "center" }}>counter</h1>
+        <div className="counterContainer" style={{ display: "flex" }}>
+          <button className="mminus" onClick={() => setCnt(cnt - 10)}>
+            &lt;&lt;
+          </button>
+          <button className="minWus" onClick={() => setCnt(cnt - 1)}>
+            -
+          </button>
+          <div
+            className="count"
+            style={{ fontSize: "20px", fontWeight: "bolder" }}
+          >
+            {cnt}
+          </div>
+          <button className="plus" onClick={() => setCnt(cnt + 1)}>
+            +
+          </button>
+          <button className="pplus" onClick={() => setCnt(cnt + 10)}>
+            &gt;&gt;
+          </button>
         </div>
-        <button className="minus" onClick={() => setCnt(cnt - 1)}>
-          -
-        </button>
       </div>
     );
   };
+  const Calculator = () => {
+    const [A, setA] = useState("");
+    const [B, setB] = useState("");
+    const [Ans, setAns] = useState("");
+    return (
+      <div>
+        <form>
+          num 1:
+          <input
+            name="A"
+            placeholder="Input 1"
+            onChange={(e) => setA(Number(e.target.value))}
+          />
+          num 2:
+          <input
+            name="B"
+            placeholder="Input 2"
+            onBlur={(e) => setB(Number(e.target.value))}
+          />
+        </form>
+        <div>
+          <button
+            onClick={() => {
+              setLogs([`${A}+${B}=${A + B}`, ...logs]);
+              setAns(A + B);
+              console.log(logs);
+              console.log(Ans);
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              setLogs([`${A}-${B}=${A - B}`, ...logs]);
+              setAns(A - B);
+              console.log(Ans);
+              console.log(logs);
+            }}
+          >
+            -
+          </button>
+          <button
+            onClick={() => {
+              setAns(A * B);
+              setLogs([`${A}*${B}=${A * B}`, ...logs]);
+              console.log(logs);
+              console.log(Ans);
+            }}
+          >
+            *
+          </button>
+          <button
+            onClick={() => {
+              setLogs([`${A}/${B}=${A / B}`, ...logs]);
+              setAns(A / B);
+              console.log(Ans);
+              console.log(logs);
+            }}
+          >
+            /
+          </button>
+        </div>
+        <div className="ans">Ans: {Ans}</div>
+        <div className="logs">
+          <p>logs</p>
+          {/* {logs.map((element, i) => {
+            return <div key={i}>{element}</div>;
+          })} */}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">React is All You Need!</header>
       <div>
-        <Hello />
-        <Counter />
+        <select onChange={(e) => setMode(e.target.value)}>
+          <option value={null}>====</option>
+          <option value={0}>counter</option>
+          <option value={1}>calculator</option>
+          <option value={2}>calender</option>
+        </select>
+      </div>
+      <div
+        className="mainContent"
+        style={{
+          margin: "20px auto",
+          padding: "30px 0",
+          width: "800px",
+          border: `1px dashed ${colors[mode]}`,
+          borderRadius: "20px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {mode == 0 ? <Counter /> : null}
+        {mode == 1 ? <Calculator /> : null}
+        {mode == 2 ? <Calculator /> : null}
       </div>
     </div>
   );
---
+++ week3/todo/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
